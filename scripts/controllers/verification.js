37c7497b82ee5bb9df4dc915036135b561ed1098
mfernandez <michelle.austria.fernandez@gmail.com>
2014-05-04 08:30:02 -0400

    username page

---
+++ scripts/controllers/verification.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('voiceUp')
+    .controller('AddCtrl', ['$scope', 'semantria', 'comments',
+        function ($scope, semantria, comments) {
+
+            log.ctrl('Main');
+
+        }]);
